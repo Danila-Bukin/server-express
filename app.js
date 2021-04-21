201ada582f6237646e7af23253c3b77a0dbeeec5
Danila-Bukin <bukin-danila@mail.ru>
2021-04-21 13:38:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ export default (express, bodyParser, createReadStream, crypto, http) => {
     const app = express();
 
     app
+    .use((r, res, next) => r.res.set({
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,POST,PUT,PATCH,OPTIONS,DELETE"})
     .use(bodyParser.urlencoded({ extended: true }))
     .get('/login/', (req, res) => res.send('bee_joo'))   
     .get('/code/', (req, res) => fs.createReadStream(import.meta.url.substring(7)).pipe(res))
