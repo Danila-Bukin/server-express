b1584f33d5f09343018a50d1850035a8075be6d9
Danila-Bukin <bukin-danila@mail.ru>
2021-04-21 13:45:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ export default (express, bodyParser, createReadStream, crypto, http) => {
     .use(bodyParser.urlencoded({ extended: true }))
     .get('/login/', (req, res) => res.send('bee_joo'))   
     .get('/code/', (req, res) => fs.createReadStream(import.meta.url.substring(7)).pipe(res))
-    .get('/sha1/:input/', r => res.send(crypto.createHash('sha1').update(r.params.input).digest('hex')));
+    .get('/sha1/:input/', r => res.send(crypto.createHash('sha1').update(r.params.input).digest('hex')))
+    .all('/*', r => r.res.send('bee_joo'));
 
     return app;
 }
